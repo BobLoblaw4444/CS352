ed7b4048bd512cff35c2936cad0ccc4e9db45252
TylerWolverton <twolvert@purdue.edu>
2014-02-24 22:31:18 -0500

    Actually did AST for real

---
+++ proj2/examples/test.java
@@ -0,0 +1,22 @@
+class BinarySearch{
+    public static void main(String[] a){
+	System.out.println(new BS().Start(20));
+    }
+}
+// This class contains an array of integers and
+// methods to initialize, print and search the array
+// using Binary Search
+
+class BS{
+    int[] number ;
+    int size ;
+
+    // Invoke methods to initialize, print and search
+    // for elements on the array
+    public int Start(int sz){
+        sz = 5 || 2 || 3;
+
+	return sz ;
+    }
+    
+}
